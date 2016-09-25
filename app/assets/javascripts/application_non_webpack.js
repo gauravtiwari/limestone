e07046a1c0f77c71bdb30f6cc6e70a5b7c12b652
Joshua Mark <joshuajmark@gmail.com>
2016-09-25 12:56:29 -0700

    Finished added Stripe, got assets loading

--- app/assets/javascripts/application_non_webpack.js
+++ app/assets/javascripts/application_non_webpack.js
@@ -1,3 +1,10 @@
 // All webpack assets in development will be loaded via webpack dev server
-
 // turbolinks comes from npm and is listed in webpack.client.base.config.js
+
+// WTF jquery, turbolinks, etc... why u no load in webpack
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require subscription
+
+console.log('application_non_webpack');
